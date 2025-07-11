903a5eb4ae9d38eb9df5456c818f1a759598ddb3
Taylor Kelley <taylorgkelley@outlook.com>
2025-07-11 13:18:22 -0500

    Some minor component changes

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <aside>
+      <div>{/* Logo */}</div>
+      <nav>{/* Menu items */}</nav>
+      <div>{/* Account */}</div>
+    </aside>
+  );
+}
+
+export default NavBar;
