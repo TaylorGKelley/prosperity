4969d0a0af40a9e77b7d4bdf8553a9c68a3632ca
Taylor Kelley <taylorgkelley@outlook.com>
2025-08-02 07:48:42 -0500

    fix: fixed bugs in auth configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,21 +3,21 @@ import { withAuth } from 'authentication-service-nextjs-sdk';
 
 // This is used to protect an entire page from a user, redirecting to /unauthorized
 const protectedPaths: Record<string, string[]> = {
-  '/budget': ['admin:read'],
+	'/budget': ['default'],
 };
 
 export default withAuth(
-  async function middleware() {
-    return NextResponse.next();
-  },
-  {
-    protectedPaths,
-  }
+	async function middleware() {
+		return NextResponse.next();
+	},
+	{
+		protectedPaths,
+	}
 );
 
 export const config = {
-  matcher: [
-    // Run on everything but Next internals and static files
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-  ],
+	matcher: [
+		// Run on everything but Next internals and static files
+		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+	],
 };
