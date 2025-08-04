2b525be08c9f7224fd344a5374299603eac80a76
Taylor Kelley <taylorgkelley@outlook.com>
2025-08-04 10:41:21 -0500

    style: made some styling changes to the NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,17 +3,20 @@
 import Link from 'next/link';
 import React from 'react';
 
+import { useAuthContext } from 'authentication-service-nextjs-sdk/client';
+import { usePathname } from 'next/navigation';
+
 const links = [
 	{
 		id: 0,
 		title: 'Home',
 		url: '/',
 	},
-	// {
-	//   id: 1,
-	//   title: 'Budget',
-	//   url: '/budget',
-	// },
+	{
+		id: 1,
+		title: 'Budget',
+		url: '/budget',
+	},
 	// {
 	//   id: 2,
 	//   title: 'wallet',
@@ -27,22 +30,28 @@ const links = [
 ] as const;
 
 function NavBar() {
-	// const { user } = useAuth();
+	const { user } = useAuthContext();
+	const pathname = usePathname();
 
 	return (
-		<nav className='p-6'>
-			{/* {user ? (
-        <span>Welcome, {`${user.firstName} ${user.lastName}`}!</span>
-      ) : (
-        <Link href={'/auth/login'}>Log in</Link>
-      )} */}
-			<ul>
+		<nav className='p-6 flex justify-between items-center'>
+			<ul className='flex gap-6'>
 				{links.map((link) => (
 					<li key={link.id}>
-						<Link href={link.url}>{link.title}</Link>
+						<Link
+							href={link.url}
+							className={`${pathname === link.url ? 'underline' : ''}`}>
+							{link.title}
+						</Link>
 					</li>
 				))}
 			</ul>
+
+			{user ? (
+				<span>Welcome, {user.email.split('@')[0]}!</span>
+			) : (
+				<Link href={'/auth/login'}>Log in</Link>
+			)}
 		</nav>
 	);
 }
