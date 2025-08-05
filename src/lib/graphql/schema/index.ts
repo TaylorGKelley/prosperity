4757f66eb88f296cbc6436095aa85cecf509cc51
Taylor Kelley <taylorgkelley@outlook.com>
2025-08-05 16:31:10 -0500

    chore: updated schema

--- src/lib/graphql/schema/index.ts
+++ src/lib/graphql/schema/index.ts
@@ -4,262 +4,304 @@ export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = {
-	[_ in K]?: never;
-};
-export type Incremental<T> =
-	| T
-	| { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-	ID: { input: string; output: string };
-	String: { input: string; output: string };
-	Boolean: { input: boolean; output: boolean };
-	Int: { input: number; output: number };
-	Float: { input: number; output: number };
-	Date: { input: any; output: any };
-	DateTime: { input: any; output: any };
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  Date: { input: any; output: any; }
+  DateTime: { input: any; output: any; }
+};
+
+export type Category = {
+  __typename?: 'Category';
+  amount: Scalars['Float']['output'];
+  endDate?: Maybe<Scalars['DateTime']['output']>;
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+  startDate: Scalars['DateTime']['output'];
+};
+
+export type CreateCategoryInput = {
+  amount: Scalars['Float']['input'];
+  endDate?: InputMaybe<Scalars['DateTime']['input']>;
+  name: Scalars['String']['input'];
+  startDate: Scalars['DateTime']['input'];
 };
 
 export type CreateTransactionInput = {
-	amount: Scalars['Float']['input'];
-	date: Scalars['DateTime']['input'];
-	description?: InputMaybe<Scalars['String']['input']>;
+  amount: Scalars['Float']['input'];
+  categoryId: Scalars['ID']['input'];
+  date: Scalars['DateTime']['input'];
+  description?: InputMaybe<Scalars['String']['input']>;
+  title: Scalars['String']['input'];
+  transactionType: TransactionType;
 };
 
 export type Mutation = {
-	__typename?: 'Mutation';
-	createTransaction: Transaction;
-	deleteTransaction: Scalars['ID']['output'];
-	updateTransaction: Transaction;
+  __typename?: 'Mutation';
+  createCategory: Category;
+  createTransaction: Transaction;
+  deleteCategory: Scalars['ID']['output'];
+  deleteTransaction: Scalars['ID']['output'];
+  updateCategory: Category;
+  updateTransaction: Transaction;
 };
+
+
+export type MutationCreateCategoryArgs = {
+  input: CreateCategoryInput;
+};
+
 
 export type MutationCreateTransactionArgs = {
-	input: CreateTransactionInput;
+  input: CreateTransactionInput;
 };
+
+
+export type MutationDeleteCategoryArgs = {
+  id: Scalars['ID']['input'];
+};
+
 
 export type MutationDeleteTransactionArgs = {
-	id: Scalars['ID']['input'];
+  id: Scalars['ID']['input'];
 };
 
+
+export type MutationUpdateCategoryArgs = {
+  input: UpdateCategoryInput;
+};
+
+
 export type MutationUpdateTransactionArgs = {
-	input: UpdateTransactionInput;
+  input: UpdateTransactionInput;
 };
 
 export type Query = {
-	__typename?: 'Query';
-	transactionById: Transaction;
-	transactions: Array<Transaction>;
+  __typename?: 'Query';
+  categories: Array<Category>;
+  categoryById: Category;
+  transactionById: Transaction;
+  transactions: Array<Transaction>;
 };
 
+
+export type QueryCategoryByIdArgs = {
+  id: Scalars['ID']['input'];
+};
+
+
 export type QueryTransactionByIdArgs = {
-	id: Scalars['ID']['input'];
+  id: Scalars['ID']['input'];
 };
 
 export type Transaction = {
-	__typename?: 'Transaction';
-	amount: Scalars['Float']['output'];
-	date: Scalars['DateTime']['output'];
-	description?: Maybe<Scalars['String']['output']>;
-	id: Scalars['ID']['output'];
+  __typename?: 'Transaction';
+  amount: Scalars['Float']['output'];
+  date: Scalars['DateTime']['output'];
+  description?: Maybe<Scalars['String']['output']>;
+  id: Scalars['ID']['output'];
+  title: Scalars['String']['output'];
+  transactionType: TransactionType;
+};
+
+export enum TransactionType {
+  BankTransfer = 'bank_transfer',
+  Cash = 'cash',
+  Check = 'check',
+  CreditCard = 'credit_card',
+  DebitCard = 'debit_card',
+  GiftCard = 'gift_card'
+}
+
+export type UpdateCategoryInput = {
+  amount: Scalars['Float']['input'];
+  endDate?: InputMaybe<Scalars['DateTime']['input']>;
+  id: Scalars['ID']['input'];
+  name: Scalars['String']['input'];
+  startDate: Scalars['DateTime']['input'];
 };
 
 export type UpdateTransactionInput = {
-	amount?: InputMaybe<Scalars['Float']['input']>;
-	date?: InputMaybe<Scalars['DateTime']['input']>;
-	description?: InputMaybe<Scalars['String']['input']>;
-	id: Scalars['ID']['input'];
+  amount?: InputMaybe<Scalars['Float']['input']>;
+  categoryId?: InputMaybe<Scalars['ID']['input']>;
+  date?: InputMaybe<Scalars['DateTime']['input']>;
+  description?: InputMaybe<Scalars['String']['input']>;
+  id: Scalars['ID']['input'];
+  title?: InputMaybe<Scalars['String']['input']>;
+  transactionType?: InputMaybe<TransactionType>;
 };
+
+
 
 export type ResolverTypeWrapper<T> = Promise<T> | T;
 
+
 export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
-	resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
 };
-export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
-	| ResolverFn<TResult, TParent, TContext, TArgs>
-	| ResolverWithResolve<TResult, TParent, TContext, TArgs>;
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = ResolverFn<TResult, TParent, TContext, TArgs> | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
 
 export type ResolverFn<TResult, TParent, TContext, TArgs> = (
-	parent: TParent,
-	args: TArgs,
-	context: TContext,
-	info: GraphQLResolveInfo,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => Promise<TResult> | TResult;
 
 export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
-	parent: TParent,
-	args: TArgs,
-	context: TContext,
-	info: GraphQLResolveInfo,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
 
 export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
-	parent: TParent,
-	args: TArgs,
-	context: TContext,
-	info: GraphQLResolveInfo,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
-export interface SubscriptionSubscriberObject<
-	TResult,
-	TKey extends string,
-	TParent,
-	TContext,
-	TArgs,
-> {
-	subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
-	resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
 }
 
 export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
-	subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
-	resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
 }
 
 export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
-	| SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
-	| SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
 
-export type SubscriptionResolver<
-	TResult,
-	TKey extends string,
-	TParent = {},
-	TContext = {},
-	TArgs = {},
-> =
-	| ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
-	| SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
 
 export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
-	parent: TParent,
-	context: TContext,
-	info: GraphQLResolveInfo,
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
 
-export type IsTypeOfResolverFn<T = {}, TContext = {}> = (
-	obj: T,
-	context: TContext,
-	info: GraphQLResolveInfo,
-) => boolean | Promise<boolean>;
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
 
 export type NextResolverFn<T> = () => Promise<T>;
 
 export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
-	next: NextResolverFn<TResult>,
-	parent: TParent,
-	args: TArgs,
-	context: TContext,
-	info: GraphQLResolveInfo,
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
+
+
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
-	Boolean: ResolverTypeWrapper<Scalars['Boolean']['output']>;
-	CreateTransactionInput: CreateTransactionInput;
-	Date: ResolverTypeWrapper<Scalars['Date']['output']>;
-	DateTime: ResolverTypeWrapper<Scalars['DateTime']['output']>;
-	Float: ResolverTypeWrapper<Scalars['Float']['output']>;
-	ID: ResolverTypeWrapper<Scalars['ID']['output']>;
-	Mutation: ResolverTypeWrapper<{}>;
-	Query: ResolverTypeWrapper<{}>;
-	String: ResolverTypeWrapper<Scalars['String']['output']>;
-	Transaction: ResolverTypeWrapper<Transaction>;
-	UpdateTransactionInput: UpdateTransactionInput;
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']['output']>;
+  Category: ResolverTypeWrapper<Category>;
+  CreateCategoryInput: CreateCategoryInput;
+  CreateTransactionInput: CreateTransactionInput;
+  Date: ResolverTypeWrapper<Scalars['Date']['output']>;
+  DateTime: ResolverTypeWrapper<Scalars['DateTime']['output']>;
+  Float: ResolverTypeWrapper<Scalars['Float']['output']>;
+  ID: ResolverTypeWrapper<Scalars['ID']['output']>;
+  Mutation: ResolverTypeWrapper<{}>;
+  Query: ResolverTypeWrapper<{}>;
+  String: ResolverTypeWrapper<Scalars['String']['output']>;
+  Transaction: ResolverTypeWrapper<Transaction>;
+  TransactionType: TransactionType;
+  UpdateCategoryInput: UpdateCategoryInput;
+  UpdateTransactionInput: UpdateTransactionInput;
 };
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
-	Boolean: Scalars['Boolean']['output'];
-	CreateTransactionInput: CreateTransactionInput;
-	Date: Scalars['Date']['output'];
-	DateTime: Scalars['DateTime']['output'];
-	Float: Scalars['Float']['output'];
-	ID: Scalars['ID']['output'];
-	Mutation: {};
-	Query: {};
-	String: Scalars['String']['output'];
-	Transaction: Transaction;
-	UpdateTransactionInput: UpdateTransactionInput;
+  Boolean: Scalars['Boolean']['output'];
+  Category: Category;
+  CreateCategoryInput: CreateCategoryInput;
+  CreateTransactionInput: CreateTransactionInput;
+  Date: Scalars['Date']['output'];
+  DateTime: Scalars['DateTime']['output'];
+  Float: Scalars['Float']['output'];
+  ID: Scalars['ID']['output'];
+  Mutation: {};
+  Query: {};
+  String: Scalars['String']['output'];
+  Transaction: Transaction;
+  UpdateCategoryInput: UpdateCategoryInput;
+  UpdateTransactionInput: UpdateTransactionInput;
 };
 
 export type AuthDirectiveArgs = {
-	permissions?: Array<Scalars['String']['input']>;
+  permissions?: Array<Scalars['String']['input']>;
 };
 
-export type AuthDirectiveResolver<
-	Result,
-	Parent,
-	ContextType = any,
-	Args = AuthDirectiveArgs,
-> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+export type AuthDirectiveResolver<Result, Parent, ContextType = any, Args = AuthDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type CategoryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Category'] = ResolversParentTypes['Category']> = {
+  amount?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
+  endDate?: Resolver<Maybe<ResolversTypes['DateTime']>, ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  startDate?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
 
 export interface DateScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['Date'], any> {
-	name: 'Date';
+  name: 'Date';
 }
 
-export interface DateTimeScalarConfig
-	extends GraphQLScalarTypeConfig<ResolversTypes['DateTime'], any> {
-	name: 'DateTime';
+export interface DateTimeScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['DateTime'], any> {
+  name: 'DateTime';
 }
 
-export type MutationResolvers<
-	ContextType = any,
-	ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation'],
-> = {
-	createTransaction?: Resolver<
-		ResolversTypes['Transaction'],
-		ParentType,
-		ContextType,
-		RequireFields<MutationCreateTransactionArgs, 'input'>
-	>;
-	deleteTransaction?: Resolver<
-		ResolversTypes['ID'],
-		ParentType,
-		ContextType,
-		RequireFields<MutationDeleteTransactionArgs, 'id'>
-	>;
-	updateTransaction?: Resolver<
-		ResolversTypes['Transaction'],
-		ParentType,
-		ContextType,
-		RequireFields<MutationUpdateTransactionArgs, 'input'>
-	>;
+export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = {
+  createCategory?: Resolver<ResolversTypes['Category'], ParentType, ContextType, RequireFields<MutationCreateCategoryArgs, 'input'>>;
+  createTransaction?: Resolver<ResolversTypes['Transaction'], ParentType, ContextType, RequireFields<MutationCreateTransactionArgs, 'input'>>;
+  deleteCategory?: Resolver<ResolversTypes['ID'], ParentType, ContextType, RequireFields<MutationDeleteCategoryArgs, 'id'>>;
+  deleteTransaction?: Resolver<ResolversTypes['ID'], ParentType, ContextType, RequireFields<MutationDeleteTransactionArgs, 'id'>>;
+  updateCategory?: Resolver<ResolversTypes['Category'], ParentType, ContextType, RequireFields<MutationUpdateCategoryArgs, 'input'>>;
+  updateTransaction?: Resolver<ResolversTypes['Transaction'], ParentType, ContextType, RequireFields<MutationUpdateTransactionArgs, 'input'>>;
 };
 
-export type QueryResolvers<
-	ContextType = any,
-	ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query'],
-> = {
-	transactionById?: Resolver<
-		ResolversTypes['Transaction'],
-		ParentType,
-		ContextType,
-		RequireFields<QueryTransactionByIdArgs, 'id'>
-	>;
-	transactions?: Resolver<Array<ResolversTypes['Transaction']>, ParentType, ContextType>;
+export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
+  categories?: Resolver<Array<ResolversTypes['Category']>, ParentType, ContextType>;
+  categoryById?: Resolver<ResolversTypes['Category'], ParentType, ContextType, RequireFields<QueryCategoryByIdArgs, 'id'>>;
+  transactionById?: Resolver<ResolversTypes['Transaction'], ParentType, ContextType, RequireFields<QueryTransactionByIdArgs, 'id'>>;
+  transactions?: Resolver<Array<ResolversTypes['Transaction']>, ParentType, ContextType>;
 };
 
-export type TransactionResolvers<
-	ContextType = any,
-	ParentType extends ResolversParentTypes['Transaction'] = ResolversParentTypes['Transaction'],
-> = {
-	amount?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
-	date?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
-	description?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
-	id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-	__isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+export type TransactionResolvers<ContextType = any, ParentType extends ResolversParentTypes['Transaction'] = ResolversParentTypes['Transaction']> = {
+  amount?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
+  date?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  description?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  transactionType?: Resolver<ResolversTypes['TransactionType'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
 export type Resolvers<ContextType = any> = {
-	Date?: GraphQLScalarType;
-	DateTime?: GraphQLScalarType;
-	Mutation?: MutationResolvers<ContextType>;
-	Query?: QueryResolvers<ContextType>;
-	Transaction?: TransactionResolvers<ContextType>;
+  Category?: CategoryResolvers<ContextType>;
+  Date?: GraphQLScalarType;
+  DateTime?: GraphQLScalarType;
+  Mutation?: MutationResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
+  Transaction?: TransactionResolvers<ContextType>;
 };
 
 export type DirectiveResolvers<ContextType = any> = {
-	auth?: AuthDirectiveResolver<any, any, ContextType>;
+  auth?: AuthDirectiveResolver<any, any, ContextType>;
 };
