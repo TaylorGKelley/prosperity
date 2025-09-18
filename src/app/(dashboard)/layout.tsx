9f256a4c106f04b7c06b2d0ee5ee833078cdad98
Taylor Kelley <taylorgkelley@outlook.com>
2025-09-18 10:43:14 -0500

    refactor: resetting to an empty slate for final design

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,15 +1,15 @@
-import { AppSidebar } from '@/components/AppSidebar';
-import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
-
 export default async function DashboardLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
 	return (
-		<SidebarProvider>
-			<AppSidebar />
-			<SidebarInset>{children}</SidebarInset>
-		</SidebarProvider>
+		<div className='grid grid-cols-[auto_var(--container-xs)] bg-gray-100 min-h-screen'>
+			<div className=''>
+				<header>logo, navbar</header>
+				<main>{children}</main>
+			</div>
+			<aside className='grid bg-white'>Sidebar</aside>
+		</div>
 	);
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,6 @@ import { Inter } from 'next/font/google';
 import { AuthProvider } from '@/context/AuthProvider';
 import { fetchWithAuth, type User } from 'authentication-service-nextjs-sdk/server';
 import './globals.css';
-import { Toaster } from '@/components/ui/sonner';
-import { ThemeProvider } from '@/context/ThemeProvider';
 
 const inter = Inter({
 	variable: '--font-inter-sans',
@@ -44,10 +42,10 @@ export default async function RootLayout({
 				<AuthProvider
 					permissions={response.success ? response.data.permissions : []}
 					user={response.success ? response.data.user : null}>
-					<ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-						{children}
-						<Toaster position='bottom-right' />
-					</ThemeProvider>
+					{/* <ThemeProvider attribute='class' defaultTheme='system' enableSystem> */}
+					{children}
+					{/* <Toaster position='bottom-right' /> */}
+					{/* </ThemeProvider> */}
 				</AuthProvider>
 			</body>
 		</html>
